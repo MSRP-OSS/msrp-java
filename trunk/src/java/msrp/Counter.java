0f7c2c6e41df8fcbee8d5f78c700957372b465b7
dtroit <dtroit@49d38564-8d68-6e85-e03b-e99df03f4e71>
2009-08-24 10:28:34 +0000

    Optimization of the Heap space and performance issue while sending big files (Issue #22) corrected. Using a bitset for the Counter class turned out to be a bad choice because the library became too slow. Not it's abandoned and a cluster with ArrayList approach is in use, more details in Project Definition .mmap
    TestSendingASCIIMessages and TestSendingBinaryMessages tests that were ignored, now aren't anymore

--- trunk/src/java/msrp/Counter.java
+++ trunk/src/java/msrp/Counter.java
@@ -20,7 +20,6 @@ import java.util.*;
 
 import org.slf4j.*;
 
-
 import msrp.messages.*;
 
 /**
@@ -39,33 +38,33 @@ public class Counter
     /**
      * The logger associated with this class
      */
-    private static final Logger logger =
-        LoggerFactory.getLogger(Counter.class);
-    
+    private static final Logger logger = LoggerFactory.getLogger(Counter.class);
+
     /**
-     * The array used to register the bytes
+     * The ArrayList used to register the bytes
      */
-    private BitSet counter;
-    
+    ArrayList<long[]> counter;
+
     private static final short STARTINGPOSITION = 0;
-    
+
+    private static final int VMIN = 0;
+
+    private static final int NRBYTESPOS = 1;
+
     /**
      * Stores the value of the index of the previous clear bit
      */
     private long indexPreviousClearBit;
-    
+
     /**
      * Stores the number of bytes that the counter has
      */
     private long count = 0;
-    
+
     /**
      * Stores the number of consecutive bytes from the start without "holes"
      */
     private long nrConsecutiveBytes = 0;
-
-
-
 
     /**
      * Creates the counter structure based on the message size
@@ -74,46 +73,159 @@ public class Counter
      */
     protected Counter(Message message)
     {
-            counter = new BitSet();
+        counter = new ArrayList<long[]>();
 
     }
 
-  
     /**
-     * Registers the given position as received
-     * updates the count number of bytes and the nr of consecutive bytes private variables
+     * Registers the given position as received updates the count number of
+     * bytes and the nr of consecutive bytes private variables
      * 
      * @param startingPosition the index position, in the overall message
      *            content that this count refers to
      * @param numberBytes the offset regarding the startingPosition to which one
      *            should count the byte
-     * @return true if there was a change on the number of contiguous bytes accounted for,
-     *         or false otherwise
+     * @return true if there was a change on the number of contiguous bytes
+     *         accounted for, or false otherwise
      */
-    protected boolean register(long startingPosition,
-        int numberBytes)
+    protected synchronized boolean register(long startingPosition,
+        long numberBytes)
     {
         logger.trace("going to register: " + numberBytes
             + " received bytes starting at position:" + startingPosition);
+        long previousValueConsecutiveBytes = nrConsecutiveBytes;
+        long[] valueToRegister =
+        { startingPosition, numberBytes };
 
-        int intStartingPosition = (int) startingPosition;
-        counter.set(intStartingPosition, intStartingPosition + numberBytes);
-        nrConsecutiveBytes = counter.nextClearBit(STARTINGPOSITION); 
-        boolean toReturn;
-        if (indexPreviousClearBit != nrConsecutiveBytes)
-            toReturn = true;
-        else
-            toReturn = false;
-        indexPreviousClearBit = nrConsecutiveBytes;
-        //update the count and nrConsecutiveBytes:
-        count = counter.cardinality();
-        return toReturn;
+        // Go through the list to find the position to insert the value
+        ListIterator<long[]> counterIterator = counter.listIterator();
+        boolean inserted = false;
+        while (counterIterator.hasNext() && !inserted)
+        {
+            long[] toEvaluate = counterIterator.next();
+            long vMaxExisting = toEvaluate[VMIN] + toEvaluate[NRBYTESPOS];
+            long vMinToInsert = valueToRegister[0];
+            long vMaxToInsert = vMinToInsert + valueToRegister[1];
+            // case one - separate cluster before
+            if (vMinToInsert < toEvaluate[VMIN]
+                && vMaxToInsert < toEvaluate[VMIN])
+            {
+                logger.debug("Case one - separate cluster before - detected");
+                // insert the new element before
+                int indexToInsert = counterIterator.previousIndex();
+                if (indexToInsert == -1)
+                    counter.add(0, valueToRegister);
+                else
+                    counter.add(indexToInsert, valueToRegister);
+                inserted = true;
+                // add the second position to the count
+                count += valueToRegister[NRBYTESPOS];
+                // update the nr consecutive bytes if it's the case:
+                if (vMinToInsert == 0)
+                    nrConsecutiveBytes = valueToRegister[NRBYTESPOS];
+                // end the cycle
+                break;
+            }// if (vMinToInsert < toEvaluate[VMIN] && vMaxtoInsert <
+            // toEvaluate[VMIN])
+            // case two - Intersects and extends it in the beginning
+            else if (vMinToInsert < toEvaluate[VMIN]
+                && vMaxExisting >= vMaxToInsert
+                && vMaxToInsert >= toEvaluate[VMIN])
+            {
+                logger.debug("Case two - Intersects and extends it in the "
+                    + " beginning - detected");
+                valueToRegister[NRBYTESPOS] =
+                    vMaxExisting - valueToRegister[VMIN];
+
+                // replace the to evaluate with this one:
+                counterIterator.remove();
+                counterIterator.add(valueToRegister);
+                inserted = true;
+                count += (toEvaluate[VMIN] - valueToRegister[VMIN]);
+                // update the nr consecutive bytes if it's the case:
+                if (vMinToInsert == 0)
+                    nrConsecutiveBytes = valueToRegister[NRBYTESPOS];
+                // end the cycle
+                break;
+            }// else if (vMinToInsert < toEvaluate[VMIN] && toEvaluate[VMAX] >=
+            // vMaxToInsert && vMaxToInsert >= toEvaluate[VMIN])
+            // case three - Within an existing cluster
+            else if (vMinToInsert >= toEvaluate[VMIN]
+                && vMaxToInsert <= vMaxExisting)
+            {
+                logger.debug("Case three - Within an existing cluster - "
+                    + "detected");
+                // don't quite do anything, just break the cycle
+                inserted = true;
+                break;
+            }
+            // case four - Intersects and extends it further to the end
+            else if (vMinToInsert >= toEvaluate[VMIN]
+                && vMinToInsert <= vMaxExisting && vMaxToInsert >= vMaxExisting)
+            {
+                logger.debug("Case four - Intersects and extends it further "
+                    + "to the end - detected");
+                // remove the existing one and create the new one
+                counterIterator.remove();
+                valueToRegister[VMIN] = toEvaluate[VMIN];
+                valueToRegister[NRBYTESPOS] =
+                    vMaxToInsert - valueToRegister[VMIN];
+                count -= toEvaluate[NRBYTESPOS];
+                if (toEvaluate[VMIN] == 0)
+                    nrConsecutiveBytes = 0;
+
+                // go to the next iteration
+                continue;
+
+            }// else if (vMinToInsert >= toEvaluate[VMIN] && vMinToInsert <=
+            // vMaxExisting && vMaxToInsert >= vMaxExisting )
+            // case five - Separate cluster after
+            else if (vMinToInsert > toEvaluate[VMIN]
+                && vMinToInsert > vMaxExisting)
+            // don't do anything
+            {
+                logger.debug("Case five - Separate cluster after - detected");
+                // go to the next iteration
+                continue;
+
+            }// else if (vMinToInsert > toEvaluate[VMIN] && vMinToInsert >
+            // vMaxExisting)
+            else if (vMinToInsert < toEvaluate[VMIN]
+                && vMaxToInsert > vMaxExisting)
+            // case six - bigger new cluster
+            {
+                logger.debug("Case six - bigger new cluster - detected");
+                // remove this one
+                counterIterator.remove();
+                count -= toEvaluate[NRBYTESPOS];
+                if (toEvaluate[VMIN] == 0)
+                    nrConsecutiveBytes = 0;
+                continue;
+            }
+            else
+            {
+                logger.error("Counter algorithm serious error, please "
+                    + "report this error to the developers");
+            }
+        }
+        if (!inserted)
+        {
+            logger.debug("Not inserted - and cycle ended - detected");
+            counter.add(valueToRegister);
+            // update the number of counted and consecutive bytes
+            count += valueToRegister[NRBYTESPOS];
+            if (valueToRegister[VMIN] == 0)
+                nrConsecutiveBytes = valueToRegister[NRBYTESPOS];
+        }
+        if (nrConsecutiveBytes != previousValueConsecutiveBytes)
+            return true;
+        return false;
     }
 
     /**
      * @return the count of bytes received
      */
-    public long getCount()
+    public synchronized long getCount()
     {
         return count;
     }
@@ -125,7 +237,7 @@ public class Counter
      * @return the number of consecutive received bytes counted from the
      *         beginning.
      */
-    protected long getNrConsecutiveBytes()
+    protected synchronized long getNrConsecutiveBytes()
     {
         return nrConsecutiveBytes;
     }
@@ -152,11 +264,11 @@ public class Counter
      *         order for a message to be complete one needs to have received the
      *         $ continuation flag and the message must not have "holes" in it)
      */
-    public boolean isComplete()
+    public synchronized boolean isComplete()
     {
         if (!dollarContinuationFlag)
             return false;
-        
+
         /* Checking for holes */
         if (count != nrConsecutiveBytes)
             return false;
