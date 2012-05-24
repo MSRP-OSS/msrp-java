967b5e64afdb0e60d73e03246f8f2c0b9b8acead
uijltje <uijltje@49d38564-8d68-6e85-e03b-e99df03f4e71>
2012-05-24 12:33:26 +0000

    [maven-release-plugin]  copy for tag msrp-1.0.1.BETA

---
+++ tags/msrp-1.0.1.BETA/src/main/java/javax/net/msrp/TransactionResponse.java
@@ -0,0 +1,181 @@
+package javax.net.msrp;
+
+import java.nio.ByteBuffer;
+
+import javax.net.msrp.exceptions.IllegalUseException;
+import javax.net.msrp.utils.TextUtils;
+
+
+/**
+ * This class represents a response to a Transaction, which is considered a
+ * transaction as well TODO use the parser to validate the response ?!
+ * 
+ * @author João André Pereira Antunes
+ * 
+ */
+public class TransactionResponse
+    extends Transaction
+{
+    protected ByteBuffer content;
+
+    protected int responseCode;
+
+    protected String comment;
+
+    /**
+     * Creates the outgoing transaction response
+     * 
+     * @param transaction the original transaction that gave birth to this
+     *            response
+     * @param responseCode the code, must be supported by the RFCs 4975 or 4976
+     * @param comment the comment as defined in RFC 4975 formal syntax,
+     *            as the comment is optional, it can also be null if no comment
+     *            is desired
+     * @param direction the direction of the transaction
+     * @throws IllegalUseException if at least one of the arguments is
+     *             incompatible
+     */
+    protected TransactionResponse(Transaction transaction, int responseCode,
+        String comment, int direction)
+        throws IllegalUseException
+    {
+        // original transaction must be a SEND transaction
+        if (transaction.transactionType != TransactionType.SEND)
+            throw new IllegalUseException(
+                "Constructing a SENDRESPONSE "
+                    + "with an original transaction that isn't a SEND, transaction: "
+                    + transaction);
+
+        this.transactionType = TransactionType.SENDRESPONSE;
+        this.direction = direction;
+        this.responseCode = responseCode;
+        this.comment = comment;
+
+        // copy the values from the original transaction to this one
+        this.message = transaction.message;
+        this.tID = transaction.tID;
+        this.transactionManager = transaction.transactionManager;
+        transaction.setResponse(this);
+
+        if (direction == IN)
+    		CreateIncomingResponse(transaction, responseCode, comment, direction);
+    	else
+    		CreateOutgoingResponse(transaction, responseCode, comment, direction);
+    }
+
+    private void CreateOutgoingResponse(Transaction transaction, int responseCode,
+            String comment, int direction)
+            throws IllegalUseException
+    {
+        // validate the response code
+        if (responseCode != 200 && responseCode != 400 && responseCode != 403
+            && responseCode != 408 && responseCode != 413
+            && responseCode != 415 && responseCode != 423
+            && responseCode != 481 && responseCode != 501
+            && responseCode != 506)
+            throw new IllegalUseException("Creating a transaction response "
+                + "with an invalid response code of: " + responseCode);
+
+        StringBuilder response = new StringBuilder(256);
+        response.append("MSRP ").append(transaction.tID).append(" ").append(responseCode);
+        if (comment != null)
+        	response.append(" ").append(comment);
+
+        response.append("\r\nTo-Path: ").append(
+                transaction.fromPath[transaction.fromPath.length - 1]);
+        response.append("\r\nFrom-Path: ").append(
+                transaction.toPath[transaction.toPath.length - 1]);
+        response.append("\r\n-------").append(transaction.tID).append("$\r\n");
+
+        this.fromPath = transaction.toPath;
+        this.toPath = transaction.fromPath;
+        byte[] contentBytes = response.toString().getBytes(TextUtils.utf8);
+        content = ByteBuffer.wrap(contentBytes);
+        content.rewind();
+    }
+
+    /**
+     * Constructor to create the incoming transaction
+     * 
+     * @param responseCode one of the response codes defined on RFC 4975
+     * @param transaction the transaction related with this
+     * @throws IllegalUseException when constructing for outgoing non-original SEND.
+     */
+    private void CreateIncomingResponse(Transaction transaction,
+        int responseCode, String comment, int direction)
+        throws IllegalUseException
+    {
+    	;
+    }
+    
+    /**
+     * 
+     * @return an int representing the number of bytes remaining for this
+     *         response
+     */
+    public int getNumberBytesRemaining()
+    {
+        return content.remaining();
+    }
+
+    /**
+     * Method that gets bulks of data (int maximum)
+     * 
+     * @param toFill byte array to be filled
+     * @param offset the offset index to start filling the outData
+     * 
+     * @return the number of bytes filled of the array
+     * @throws IndexOutOfBoundsException if the offset is bigger than the array
+     *             length
+     */
+    @Override
+    public int getData(byte[] toFill, int offset)
+        throws IndexOutOfBoundsException
+    {
+        int remainingBytes = content.remaining();
+        int lengthToTransfer = 0;
+        if ((toFill.length - offset) > remainingBytes)
+            lengthToTransfer = remainingBytes;
+        else
+            lengthToTransfer = (toFill.length - offset);
+        content.get(toFill, 0, lengthToTransfer);
+        return lengthToTransfer;
+    }
+
+    @Override
+    public byte get()
+    {
+        return content.get();
+    }
+
+    @Override
+    public boolean hasData()
+    {
+        return content.hasRemaining();
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Transaction response of Tx[" + tID + "] response code["
+            + responseCode + "]";
+    }
+
+    /**
+     * Seen that we use the content field to put the end line we will always
+     * return false on this method call
+     */
+    @Override
+    public boolean hasEndLine()
+    {
+        return false;
+    }
+
+    @Override
+    protected boolean isIncomingResponse()
+    {
+        if (direction == IN)
+            return true;
+        return false;
+    }
+}
---
+++ tags/msrp-1.0.1.BETA/src/main/java/javax/net/msrp/testutils/MockMSRPSessionListener.java
@@ -0,0 +1,359 @@
+/*
+ * Copyright © João Antunes 2008 This file is part of MSRP Java Stack.
+ * 
+ * MSRP Java Stack is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * MSRP Java Stack is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MSRP Java Stack. If not, see <http://www.gnu.org/licenses/>.
+ */
+package javax.net.msrp.testutils;
+
+import java.util.ArrayList;
+
+import javax.net.msrp.DataContainer;
+import javax.net.msrp.MSRPSessionListener;
+import javax.net.msrp.MemoryDataContainer;
+import javax.net.msrp.Session;
+import javax.net.msrp.Transaction;
+import javax.net.msrp.events.MessageAbortedEvent;
+import javax.net.msrp.messages.IncomingMessage;
+import javax.net.msrp.messages.Message;
+import javax.net.msrp.messages.OutgoingMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Class used to test the callbacks of the MSRPStack
+ * 
+ * @author João André Pereira Antunes 2008
+ * 
+ */
+public class MockMSRPSessionListener
+    implements MSRPSessionListener
+{
+
+    /**
+     * The logger associated with this class
+     */
+    private static final Logger logger =
+        LoggerFactory.getLogger(MockMSRPSessionListener.class);
+
+    /* The field results of the calls: */
+    private Session acceptHookSession;
+
+    private Message acceptHookMessage;
+
+    private Session receiveMessageSession;
+
+    private Message receiveMessage;
+
+    private Session receivedReportSession;
+
+    private Transaction receivedReportTransaction;
+
+    private Session updateSendStatusSession;
+
+    private Message updateSendStatusMessage;
+
+    private Session abortedMessageSession;
+
+    private Message abortedMessage;
+
+    private Boolean acceptHookResult;
+
+    /**
+     * The external to be set, or not data container object that is going to be
+     * used on the acceptHook if it exists upon the calling of the trigger
+     */
+    private DataContainer externalDataContainer;
+
+    /**
+     * Counter for the send status update, the number of elements reflect the
+     * number of calls to the updateSendStatus method and its values the number
+     * of bytes that the function was called with
+     * 
+     * @see MSRPSessionListener#updateSendStatus(Session, Message, long)
+     */
+    public ArrayList<Long> updateSendStatusCounter = new ArrayList<Long>();
+
+    /**
+     * A counter for the number of success reports received
+     */
+    public ArrayList<Long> successReportCounter = new ArrayList<Long>();
+
+    /**
+     * A counter for the number of message aborts received. the size of this
+     * array counts how many message abortions and its values the bytes that
+     * were received before the message was aborted
+     */
+    public ArrayList<Long> abortMessageCounter = new ArrayList<Long>();
+
+    public ArrayList<MessageAbortedEvent> messageAbortEvents =
+    new ArrayList<MessageAbortedEvent>();
+
+    /**
+     * Constructor of the mock session listener
+     * 
+     * @param name the String that names this constructor, used for debug
+     *            purposes
+     */
+    public MockMSRPSessionListener(String name)
+    {
+    }
+
+    @Override
+    public boolean acceptHook(Session session, IncomingMessage message)
+    {
+        logger.trace("AcceptHook called");
+        while (acceptHookResult == null)
+        {
+            synchronized (this)
+            {
+                try
+                {
+                    wait();
+                }
+                catch (InterruptedException e)
+                {
+                    throw new RuntimeException();
+                }
+            }
+        }
+        boolean toReturn = acceptHookResult.booleanValue();
+
+        logger.debug("AcceptHook will return: " + toReturn);
+
+        acceptHookMessage = message;
+        acceptHookSession = session;
+        if (externalDataContainer == null)
+        {
+            MemoryDataContainer dataContainer =
+                new MemoryDataContainer((int) message.getSize());
+            message.setDataContainer(dataContainer);
+        }
+        else
+        {
+            message.setDataContainer(externalDataContainer);
+        }
+        acceptHookResult = null;
+        synchronized (this)
+        {
+            this.notifyAll();
+            logger.debug("AcceptHook returns: " + toReturn);
+            return toReturn;
+        }
+    }
+
+    @Override
+    public void receiveMessage(Session session, Message message)
+    {
+        logger.debug("receiveMessage(id=[" + message.getMessageID() + "])");
+        receiveMessage = message;
+        receiveMessageSession = session;
+        /* 
+         * delay this thread to allow the other thread to be scheduled
+         * before acquiring a lock again.
+         */
+        try { Thread.sleep(100); } catch (InterruptedException e) { ; }
+
+        synchronized (this)
+        {
+            this.notifyAll();
+        }
+    }
+
+    @Override
+    public void receivedReport(Session session, Transaction tReport)
+    {
+        if (tReport.getStatusHeader().getStatusCode() != 200)
+        {
+            logger.debug("Received report with code different from 200"
+                + ", with code: " + tReport.getStatusHeader().getStatusCode()
+                + " returning");
+            return;
+        }
+        logger.debug("Received report, confirming "
+            + tReport.getByteRange()[1] + " bytes were sent(== "
+            + (tReport.getByteRange()[1] * 100)
+            / tReport.getTotalMessageBytes() + "%) Tx-"
+            + tReport.transactionType + ", status:"
+            + tReport.getStatusHeader().getStatusCode());
+        receivedReportSession = session;
+        receivedReportTransaction = tReport;
+        synchronized (successReportCounter)
+        {
+            successReportCounter.add(tReport.getByteRange()[1]);
+            successReportCounter.notifyAll();
+        }
+    }
+
+    @Override
+    public void updateSendStatus(Session session, Message message,
+        long numberBytesSent)
+    {
+    	final long size = message.getSize();
+        logger.debug("updateSendStatus() bytes sent: " + numberBytesSent
+            + " == " + (size == 0 ? 100 : (numberBytesSent * 100) / size) + "%");
+        updateSendStatusSession = session;
+        updateSendStatusMessage = message;
+        synchronized (updateSendStatusCounter)
+        {
+            updateSendStatusCounter.add(numberBytesSent);
+            updateSendStatusCounter.notifyAll();
+        }
+    }
+
+    @Override
+    public void abortedMessageEvent(MessageAbortedEvent abortEvent)
+    {
+    	final long size;
+        messageAbortEvents.add(abortEvent);
+        boolean incoming = false;
+        if (abortEvent.getMessage().getDirection() == Message.IN)
+        {
+            incoming = true;
+
+            IncomingMessage message = (IncomingMessage) abortEvent.getMessage();
+        	size = message.getSize();
+            logger.debug("abortedMessageEvent() bytes received: "
+                + message.getReceivedBytes() + " == "
+                + (size == 0 ? 100 : (message.getReceivedBytes() * 100) / size) + "%");
+        }
+        else if (abortEvent.getMessage().getDirection() == Message.OUT)
+        {
+            OutgoingMessage message = (OutgoingMessage) abortEvent.getMessage();
+        	size = message.getSize();
+            logger.debug("abortedMessageEvent() bytes sent: "
+                + message.getSentBytes() + " == "
+                + (size == 0 ? 100 : (message.getSentBytes() * 100) / size) + "%");
+        }
+        abortedMessage = abortEvent.getMessage();
+        abortedMessageSession = abortEvent.getSession();
+        synchronized (abortMessageCounter)
+        {
+            if (incoming)
+                abortMessageCounter.add(((IncomingMessage) abortedMessage)
+                    .getReceivedBytes());
+            else
+                abortMessageCounter.add(((OutgoingMessage) abortedMessage)
+                    .getSentBytes());
+            abortMessageCounter.notifyAll();
+        }
+    }
+
+    @Override
+    public void abortedMessage(Session session, IncomingMessage message)
+    {
+        // Deprecated
+
+    }
+
+    /**
+     * @return the acceptHookSession
+     */
+    public Session getAcceptHookSession()
+    {
+        return acceptHookSession;
+    }
+
+    /**
+     * @return the acceptHookMessage
+     */
+    public Message getAcceptHookMessage()
+    {
+        return acceptHookMessage;
+    }
+
+    /**
+     * @return the receiveMessageSession
+     */
+    public Session getReceiveMessageSession()
+    {
+        return receiveMessageSession;
+    }
+
+    /**
+     * @return the receiveMessage
+     */
+    public Message getReceiveMessage()
+    {
+        return receiveMessage;
+    }
+
+    /**
+     * @return the receivedReportSession
+     */
+    public Session getReceivedReportSession()
+    {
+        return receivedReportSession;
+    }
+
+    /**
+     * @return the receivedReportTransaction
+     */
+    public Transaction getReceivedReportTransaction()
+    {
+        return receivedReportTransaction;
+    }
+
+    /**
+     * @return the updateSendStatusSession
+     */
+    public Session getUpdateSendStatusSession()
+    {
+        return updateSendStatusSession;
+    }
+
+    /**
+     * @return the updateSendStatusMessage
+     */
+    public Message getUpdateSendStatusMessage()
+    {
+        return updateSendStatusMessage;
+    }
+
+    /**
+     * @return the abortedMessageSession
+     */
+    public Session getAbortedMessageSession()
+    {
+        return abortedMessageSession;
+    }
+
+    /**
+     * @return the abortedMessage
+     */
+    public Message getAbortedMessage()
+    {
+        return abortedMessage;
+    }
+
+    /**
+     * @param acceptHookResult the acceptHookResult to set
+     */
+    public void setAcceptHookResult(Boolean acceptHookResult)
+    {
+        this.acceptHookResult = acceptHookResult;
+    }
+
+    public void setDataContainer(DataContainer dc)
+    {
+        externalDataContainer = dc;
+    }
+
+	@Override
+	public void connectionLost(Session session, Throwable cause) {
+		logger.warn("Connection broke, reason: " + cause.getMessage());
+		session.tearDown();
+	}
+}
