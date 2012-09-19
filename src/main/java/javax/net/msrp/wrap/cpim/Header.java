e11b0e87c0bc34dd4253054c61c4f31f0b05db7e
Tom Uijldert <tom.uijldert@gmail.com>
2012-09-19 10:06:13 +0000

    Enhancement: improved wrapped message interfacing.

--- src/main/java/javax/net/msrp/wrap/cpim/Header.java
+++ src/main/java/javax/net/msrp/wrap/cpim/Header.java
@@ -23,17 +23,6 @@ package javax.net.msrp.wrap.cpim;
 public class Header {
 	public static final String CPIM_TYPE = "message/cpim";
 
-	public static final String CONTENT_TYPE = "Content-type";
-	public static final String FROM = "From";
-	public static final String TO = "To";
-	public static final String CC = "cc";
-	public static final String DATETIME = "DateTime";
-	public static final String SUBJECT = "Subject";
-	public static final String NS = "NS";
-	public static final String CONTENT_LENGTH = "Content-length";
-	public static final String REQUIRE = "Require";
-	public static final String CONTENT_DISPOSITION = "Content-Disposition";
-
 	/**
 	 * Header name
 	 */
