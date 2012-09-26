684301afa5f66e75b89b5d3980664d05e64644e1
uijltje <uijltje@49d38564-8d68-6e85-e03b-e99df03f4e71>
2012-09-26 11:43:40 +0000

    Refactor: c# modification - move content-type definition to Message (no fields in interface definition).

--- trunk/src/main/java/javax/net/msrp/StatusMessage.java
+++ trunk/src/main/java/javax/net/msrp/StatusMessage.java
@@ -8,9 +8,6 @@ package javax.net.msrp;
  * @author tuijldert
  */
 public interface StatusMessage {
-	/** content-type of an isComposing message	*/
-	public static final String IMCOMPOSE_TYPE = "application/im-iscomposing+xml";
-
 	public ImState getState();
 	public String getComposeContentType();
 	public long getLastActive();
